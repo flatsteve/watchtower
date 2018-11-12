ff1a40874db0a704a48a93571e94c940ed8eeb28
Stephen McCann <info@flatsteve.com>
2018-11-12 21:54:03 +0000

    Tidy and sent correct time

--- utils.py
+++ utils.py
@@ -5,28 +5,38 @@ import requests
 from db import db, Settings
 from gpiozero import Buzzer, LED
 
+
 buzzer = Buzzer(17)
 led = LED(26)
 
+
 def reset():
+    config.alerts_triggered = 0
+
     try:
         led.off()
         buzzer.off()
     except: 
         pass
 
+
 def get_images():
     images = glob.glob('./static/camera-images/*.jpg')
     return sorted(images, reverse=True)
 
+
 def get_settings():
     return Settings.query.get(1)
 
+
 def save_settings(settings):
     current_settings = Settings.query.get(1)
+    
     current_settings.iso = settings.get('iso', 0)
     current_settings.shutter_speed = settings.get('shutter_speed', 0)
+    
     db.session.commit()
+
 
 def remove_image(url):
     try:
@@ -34,8 +44,10 @@ def remove_image(url):
     except OSError:
         print('Failed to remove image')
 
+
 def startup():
     reset()
+    
     print('Starting Watchtower in...')
 
     for i in range(config.startup_delay, 0, -1):
@@ -49,6 +61,7 @@ def startup():
             print('Problem with pins')
             pass
 
+
 def toggle():
     print('Toggling system')
     if not config.system_active:
@@ -56,8 +69,8 @@ def toggle():
         config.system_active = True
     else:
         config.system_active = False
-        config.alerts_triggered = 0
         reset()
+
 
 def flash():
     try:
@@ -71,23 +84,26 @@ def flash():
         print('Problem with pins')
         pass
 
+
 def alert():
     if config.alerts_triggered >= config.max_alerts:
         return
     try:
-        r = requests.post("https://hooks.zapier.com/hooks/catch/2853925/8shq4l/", data={'time': time.time()})
+        r = requests.post(config.zap_url, data={'time': datetime.datetime.now().strftime("%H:%M")})
         print(r.status_code, r.reason)
     except Exception:
         print("Somthing went wrong...")
         pass
-    
+  
     # Avoid circular dependancy 
     from camera import take_picture
     take_picture()
     flash()
     config.alerts_triggered += 1
 
+
 def shutdown():
     print 'Sensor shutting down...'
     reset()
     sys.exit()
+
